d40a80513d9a7311c225939e67d1e4930f6dc656
LUAN BRENDON <luan_brendonmacedo@hotmail.com>
2024-06-13 23:48:54 -0300

    estilizacao e importacao de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CardComponent } from './component/card/card.component';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './component/header/header.component';
+import { HomeComponent } from './component/home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , CardComponent, CommonModule, HeaderComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
