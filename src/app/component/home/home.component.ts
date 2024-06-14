a41d2646ec9f83f47bdbe3ddcf611f973f409d05
LUAN BRENDON <luan_brendonmacedo@hotmail.com>
2024-06-13 23:50:05 -0300

    mudancas no home

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
-
+import { CardComponent } from '../card/card.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
